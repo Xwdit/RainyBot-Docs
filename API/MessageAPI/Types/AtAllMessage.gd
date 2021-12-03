001047281eb039d9b0fbb861988aca21ebd65473
Xwdit <xwditfr@gmail.com>
2021-12-03 14:21:29 +0100

    完成部分消息类型的文档编写

--- API/MessageAPI/Types/AtAllMessage.gd
+++ API/MessageAPI/Types/AtAllMessage.gd
@@ -1,12 +1,28 @@
-extends Message
+extends Message #继承Message类，用于RainyBot内部处理与加载，请勿进行改动
 
 
-class_name AtAllMessage
+##
+## RainyBot的At全体成员消息类，通常代表一个对应实例，此类消息仅适用于群聊
+##
+## @desc:
+##     这是RainyBot的At全体成员消息类，通常代表一个对应实例，此类消息仅适用于群聊
+##     此类实例代表消息对应的群聊中的At全体成员事件，你也可以主动构建此类实例以便进行此操作
+##
 
 
+class_name AtAllMessage #定义类名为AtAllMessage，请勿进行改动
+
+
+## 手动构造一个AtAllMessage类的实例，以便进行At全体成员操作
+static func init()->AtAllMessage:
+	return null
+
+
+## 通过机器人协议后端的元数据字典构造一个AtAllMessage类的实例，仅当你知道自己在做什么时才使用
 static func init_meta(dic:Dictionary)->AtAllMessage:
 	return null
 
 
+## 将此实例获取为字符串的形式
 func get_as_text()->String:
 	return "[@全体成员]"
